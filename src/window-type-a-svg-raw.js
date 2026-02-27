1e266e6b8e210f5f9b56fcccff66bf9226eb8f25
jpbrewer <jpbrewer@yahoo.com>
2026-02-27 14:45:38 -0500

    Add SVG template
    So this file is the SVG template that the SVG builder code uses to create all of the building block elements for the combination unit.
    
    It's important to know that this SVG was created from an actual set of raw data inside a.svg file created by Inkscape, but it's important that that raw svg get put through something like ChatGPT to make it compatible with including that text in a JavaScript function, because the quotation marks and other certain designations differ between the file format of an SVG and how it needs to be portrayed in a text string in JavaScript.

---
+++ src/window-type-a-svg-raw.js
@@ -0,0 +1,152 @@
+  // Window Type A SVG template (Inkscape export)
+  // Safe to paste into Webflow embed / custom code.
+  window.WINDOW_TYPE_A_SVG_TEXT = `<?xml version="1.0" encoding="UTF-8" standalone="no"?>
+<!-- Created with Inkscape (http://www.inkscape.org/) -->
+
+<svg
+   width="14000"
+   height="14000"
+   viewBox="0 0 14000 14000"
+   version="1.1"
+   id="svg1"
+   xmlns="http://www.w3.org/2000/svg"
+   xmlns:svg="http://www.w3.org/2000/svg">
+  <defs
+     id="defs1" />
+  <g
+     id="render_root">
+    <g
+       id="glass_area">
+      <rect
+         style="fill:#000000;fill-opacity:0.2;stroke:none;stroke-width:1.94715;stroke-dasharray:none;stroke-opacity:1"
+         id="daylight_rect"
+         width="2544"
+         height="816"
+         x="794.05243"
+         y="416.25494" />
+    </g>
+    <g
+       id="bevel_prototypes">
+      <rect
+         style="display:inline;fill:#000000;fill-opacity:0.431122;stroke:none;stroke-width:0.2;stroke-dasharray:none;stroke-opacity:1"
+         id="bevel_lite_proto"
+         width="1440"
+         height="384"
+         x="1898.0525"
+         y="416.25494" />
+      <path
+         id="bevel_right_proto"
+         style="fill:#000000;fill-opacity:0.431122;stroke-width:0.755906"
+         d="m 3338.0525,416.25494 -36,36 v 312 l 36,36 z" />
+      <path
+         id="bevel_left_proto"
+         style="fill:#000000;fill-opacity:0.431122;stroke-width:0.755906"
+         d="m 1898.0525,416.25494 v 384 l 36,-36 v -312 z" />
+      <path
+         id="bevel_bottom_proto"
+         style="fill:#000000;fill-opacity:0.431122;stroke-width:0.755906"
+         d="m 1934.0527,764.25586 -36,36 h 1440 l -36,-36 z" />
+      <path
+         id="bevel_top_proto"
+         style="fill:#000000;fill-opacity:0.431122;stroke-width:0.755906"
+         d="m 1898.0527,416.25586 36,36 h 1368 l 36,-36 z" />
+    </g>
+    <g
+       id="muntin_prototypes">
+      <rect
+         style="fill:#000000;fill-opacity:0.497449;stroke:none;stroke-width:0.264999;stroke-dasharray:none;stroke-opacity:1"
+         id="muntin_horizontal_proto"
+         width="2544"
+         height="42"
+         x="794.05249"
+         y="800.25494" />
+      <rect
+         style="fill:#000000;fill-opacity:0.497449;stroke:none;stroke-width:0.272924;stroke-dasharray:none;stroke-opacity:1"
+         id="muntin_vertical_proto"
+         width="42"
+         height="816"
+         x="1856.0525"
+         y="416.25494" />
+    </g>
+    <g
+       id="sash">
+      <rect
+         style="fill:#000000;fill-opacity:1;stroke:none;stroke-width:1.94832;stroke-dasharray:none;stroke-opacity:1"
+         id="stile_right"
+         width="168"
+         height="816"
+         x="3338.0525"
+         y="416.25494" />
+      <rect
+         style="fill:#000000;fill-opacity:1;stroke:none;stroke-width:1.94832;stroke-dasharray:none;stroke-opacity:1"
+         id="stile_left"
+         width="168"
+         height="816"
+         x="626.05249"
+         y="416.25494" />
+      <rect
+         style="fill:#000000;fill-opacity:1;stroke:none;stroke-width:0.265;stroke-dasharray:none;stroke-opacity:1"
+         id="rail_bottom"
+         width="2880"
+         height="168"
+         x="626.05249"
+         y="1232.2549" />
+      <rect
+         style="fill:#000000;fill-opacity:1;stroke:none;stroke-width:0.264583;stroke-opacity:1"
+         id="rail_top"
+         width="2880"
+         x="626.05249"
+         y="248.25493"
+         height="168" />
+      <rect
+         style="fill:none;stroke:#000000;stroke-width:0.264583;stroke-opacity:1"
+         id="outside_boundary_sash"
+         height="1152"
+         x="626.05237"
+         y="248.25493"
+         width="2880" />
+    </g>
+    <g
+       id="unit">
+      <rect
+         style="fill:none;stroke:#000000;stroke-width:0.264583;stroke-opacity:1"
+         id="outside_boundary_unit"
+         height="1296"
+         x="554.05225"
+         y="176.25491"
+         width="3024" />
+      <rect
+         style="fill:#000000;fill-opacity:0.535714;stroke:none;stroke-width:1.94832;stroke-dasharray:none;stroke-opacity:1"
+         id="jamb_right"
+         width="72"
+         height="1152"
+         x="554.05231"
+         y="248.25493" />
+      <rect
+         style="fill:#000000;fill-opacity:0.535714;stroke:none;stroke-width:1.94832;stroke-dasharray:none;stroke-opacity:1"
+         id="jamb_left"
+         width="72"
+         height="1152"
+         x="3506.0522"
+         y="248.25493" />
+      <rect
+         style="fill:#000000;fill-opacity:0.649351;stroke:none;stroke-width:0.264583;stroke-opacity:1"
+         id="jamb_bottom"
+         width="3024"
+         x="554.05231"
+         y="1400.255"
+         height="72" />
+      <rect
+         style="fill:#000000;fill-opacity:0.649351;stroke:none;stroke-width:0.264583;stroke-opacity:1"
+         id="jamb_top"
+         width="3024"
+         x="554.05231"
+         y="176.25491"
+         height="72" />
+    </g>
+  </g>
+</svg>
+`;
+
+  // (optional quick sanity check)
+  // console.log("WINDOW_TYPE_A_SVG_TEMPLATE length:", window.WINDOW_TYPE_A_SVG_TEMPLATE.length);
